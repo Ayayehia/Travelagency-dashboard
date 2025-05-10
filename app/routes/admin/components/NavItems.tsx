c91bf033c6ca6fd60df36fbdbd029e3baa6f2b7b
ayayehia <ayanader8@gmail.com>
2025-05-10 23:01:47 +0300

    style: update user avatar section in NavItems component

--- app/routes/admin/components/NavItems.tsx
+++ app/routes/admin/components/NavItems.tsx
@@ -27,13 +27,36 @@ export const NavItems = () => {
                       isActive ? "brightness-0 invert" : "text-dark-200"
                     }`}
                   />
-                  <span className="text-sm">{label}</span>
+                  <span>{label}</span>
                 </div>
               )}
             </NavLink>
           </div>
         ))}
       </div>
+      {/*  */}
+      <div className="flex flex-col gap-4 mt-auto mb-6">
+        <div className="flex h-full">
+          <img
+            src="/assets/icons/users.svg"
+            alt="User avatar"
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <div className="flex flex-col gap-1">
+            <h2>Aya Nader</h2>
+            <p>aya@gmail.com</p>
+          </div>
+          <button>
+            <img
+              src="/assets/icons/logout.svg"
+              alt="Logout"
+              className="w-5 h-5"
+            />
+          </button>
+        </div>
+      </div>
+
+      {/*  */}
     </section>
   );
 };
